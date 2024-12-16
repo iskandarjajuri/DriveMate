dc79bfc8c33578c72372224d6275063ee8e14abd
iskandarjajuri <raffardhanagiska@gmail.com>
2024-12-16 15:42:54 +0700

    Initial Commit

---
+++ DriveMate_New/DriveMate_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveMate_NewApp.swift
+//  DriveMate_New
+//
+//  Created by iskandar jajuri on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DriveMate_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DriveMate_NewTests/DriveMate_NewTests.swift
@@ -0,0 +1,16 @@
+//
+//  DriveMate_NewTests.swift
+//  DriveMate_NewTests
+//
+//  Created by iskandar jajuri on 16/12/24.
+//
+
+import Testing
+
+struct DriveMate_NewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
